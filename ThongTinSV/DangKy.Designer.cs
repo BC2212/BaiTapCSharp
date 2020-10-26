70d5b41311e32edb6efca56cde1430cc3ab4f5c4
Linh Anything <huynhquanglinhpy2016@gmail.com>
2020-10-26 12:49:14 +0700

    form tìm kiếm, sửa các name ở form Đăng Ký

--- ThongTinSV/DangKy.Designer.cs
+++ ThongTinSV/DangKy.Designer.cs
@@ -30,24 +30,24 @@
         {
             this.lblTitleDangKi = new System.Windows.Forms.Label();
             this.lnlHo = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtHo = new System.Windows.Forms.TextBox();
             this.lblTen = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
             this.lblSDT = new System.Windows.Forms.Label();
             this.lblEmail = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.lblTaiKhoan = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtTaiKhoan = new System.Windows.Forms.TextBox();
             this.lblMatKhau = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
             this.lblTitleNgaySinh = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtNgay = new System.Windows.Forms.TextBox();
             this.lblNgay = new System.Windows.Forms.Label();
             this.lblThang = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.txtThang = new System.Windows.Forms.TextBox();
+            this.lblNam = new System.Windows.Forms.Label();
             this.chkNam = new System.Windows.Forms.CheckBox();
             this.chkNu = new System.Windows.Forms.CheckBox();
             this.btnDangKy = new System.Windows.Forms.Button();
@@ -75,14 +75,14 @@
             this.lnlHo.Text = "Họ";
             this.lnlHo.Click += new System.EventHandler(this.lnlHo_Click);
             // 
-            // textBox1
+            // txtHo
             // 
-            this.textBox1.Location = new System.Drawing.Point(56, 120);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(170, 40);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtHo.Location = new System.Drawing.Point(56, 120);
+            this.txtHo.Multiline = true;
+            this.txtHo.Name = "txtHo";
+            this.txtHo.Size = new System.Drawing.Size(170, 40);
+            this.txtHo.TabIndex = 3;
+            this.txtHo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // lblTen
             // 
@@ -94,21 +94,21 @@
             this.lblTen.TabIndex = 4;
             this.lblTen.Text = "Tên";
             // 
-            // textBox2
+            // txtTen
             // 
-            this.textBox2.Location = new System.Drawing.Point(282, 120);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(170, 40);
-            this.textBox2.TabIndex = 5;
+            this.txtTen.Location = new System.Drawing.Point(282, 120);
+            this.txtTen.Multiline = true;
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(170, 40);
+            this.txtTen.TabIndex = 5;
             // 
-            // textBox3
+            // txtSDT
             // 
-            this.textBox3.Location = new System.Drawing.Point(111, 170);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(341, 40);
-            this.textBox3.TabIndex = 6;
+            this.txtSDT.Location = new System.Drawing.Point(111, 170);
+            this.txtSDT.Multiline = true;
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(341, 40);
+            this.txtSDT.TabIndex = 6;
             // 
             // lblSDT
             // 
@@ -130,13 +130,13 @@
             this.lblEmail.TabIndex = 8;
             this.lblEmail.Text = "Email";
             // 
-            // textBox4
+            // txtEmail
             // 
-            this.textBox4.Location = new System.Drawing.Point(111, 220);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(341, 40);
-            this.textBox4.TabIndex = 9;
+            this.txtEmail.Location = new System.Drawing.Point(111, 220);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(341, 40);
+            this.txtEmail.TabIndex = 9;
             // 
             // lblTaiKhoan
             // 
@@ -148,13 +148,13 @@
             this.lblTaiKhoan.TabIndex = 10;
             this.lblTaiKhoan.Text = "Tài khoản";
             // 
-            // textBox5
+            // txtTaiKhoan
             // 
-            this.textBox5.Location = new System.Drawing.Point(111, 270);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(341, 40);
-            this.textBox5.TabIndex = 11;
+            this.txtTaiKhoan.Location = new System.Drawing.Point(111, 270);
+            this.txtTaiKhoan.Multiline = true;
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.Size = new System.Drawing.Size(341, 40);
+            this.txtTaiKhoan.TabIndex = 11;
             // 
             // lblMatKhau
             // 
@@ -166,13 +166,13 @@
             this.lblMatKhau.TabIndex = 12;
             this.lblMatKhau.Text = "Mật khẩu";
             // 
-            // textBox6
+            // txtMatKhau
             // 
-            this.textBox6.Location = new System.Drawing.Point(111, 320);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(341, 40);
-            this.textBox6.TabIndex = 13;
+            this.txtMatKhau.Location = new System.Drawing.Point(111, 320);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(341, 40);
+            this.txtMatKhau.TabIndex = 13;
             // 
             // lblTitleNgaySinh
             // 
@@ -184,13 +184,13 @@
             this.lblTitleNgaySinh.TabIndex = 14;
             this.lblTitleNgaySinh.Text = "Ngày Sinh";
             // 
-            // textBox7
+            // txtNgay
             // 
-            this.textBox7.Location = new System.Drawing.Point(80, 385);
-            this.textBox7.Multiline = true;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(80, 30);
-            this.textBox7.TabIndex = 15;
+            this.txtNgay.Location = new System.Drawing.Point(80, 385);
+            this.txtNgay.Multiline = true;
+            this.txtNgay.Name = "txtNgay";
+            this.txtNgay.Size = new System.Drawing.Size(80, 30);
+            this.txtNgay.TabIndex = 15;
             // 
             // lblNgay
             // 
@@ -212,32 +212,32 @@
             this.lblThang.TabIndex = 17;
             this.lblThang.Text = "Tháng";
             // 
-            // textBox8
+            // txtNam
             // 
-            this.textBox8.Location = new System.Drawing.Point(372, 385);
-            this.textBox8.Multiline = true;
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(80, 30);
-            this.textBox8.TabIndex = 18;
-            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            this.txtNam.Location = new System.Drawing.Point(372, 385);
+            this.txtNam.Multiline = true;
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(80, 30);
+            this.txtNam.TabIndex = 18;
+            this.txtNam.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
             // 
-            // textBox9
+            // txtThang
             // 
-            this.textBox9.Location = new System.Drawing.Point(226, 385);
-            this.textBox9.Multiline = true;
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(80, 30);
-            this.textBox9.TabIndex = 19;
+            this.txtThang.Location = new System.Drawing.Point(226, 385);
+            this.txtThang.Multiline = true;
+            this.txtThang.Name = "txtThang";
+            this.txtThang.Size = new System.Drawing.Size(80, 30);
+            this.txtThang.TabIndex = 19;
             // 
-            // label1
+            // lblNam
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(312, 390);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 20);
-            this.label1.TabIndex = 20;
-            this.label1.Text = "Tháng";
+            this.lblNam.AutoSize = true;
+            this.lblNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNam.Location = new System.Drawing.Point(324, 390);
+            this.lblNam.Name = "lblNam";
+            this.lblNam.Size = new System.Drawing.Size(42, 20);
+            this.lblNam.TabIndex = 20;
+            this.lblNam.Text = "Năm";
             // 
             // chkNam
             // 
@@ -293,24 +293,24 @@
             this.Controls.Add(this.btnDangKy);
             this.Controls.Add(this.chkNu);
             this.Controls.Add(this.chkNam);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.lblNam);
+            this.Controls.Add(this.txtThang);
+            this.Controls.Add(this.txtNam);
             this.Controls.Add(this.lblThang);
             this.Controls.Add(this.lblNgay);
-            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.txtNgay);
             this.Controls.Add(this.lblTitleNgaySinh);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.txtMatKhau);
             this.Controls.Add(this.lblMatKhau);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.txtTaiKhoan);
             this.Controls.Add(this.lblTaiKhoan);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.lblSDT);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtTen);
             this.Controls.Add(this.lblTen);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtHo);
             this.Controls.Add(this.lnlHo);
             this.Controls.Add(this.lblSlogen);
             this.Controls.Add(this.lblTitleDangKi);
@@ -325,24 +325,24 @@
 
         private System.Windows.Forms.Label lblTitleDangKi;
         private System.Windows.Forms.Label lnlHo;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtHo;
         private System.Windows.Forms.Label lblTen;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtSDT;
         private System.Windows.Forms.Label lblSDT;
         private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Label lblTaiKhoan;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtTaiKhoan;
         private System.Windows.Forms.Label lblMatKhau;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtMatKhau;
         private System.Windows.Forms.Label lblTitleNgaySinh;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox txtNgay;
         private System.Windows.Forms.Label lblNgay;
         private System.Windows.Forms.Label lblThang;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.TextBox txtThang;
+        private System.Windows.Forms.Label lblNam;
         private System.Windows.Forms.CheckBox chkNam;
         private System.Windows.Forms.CheckBox chkNu;
         private System.Windows.Forms.Button btnDangKy;
---
+++ ThongTinSV/TimKiem1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ThongTinSV
+{
+    partial class TimKiem1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimKiem1));
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(12, 12);
+            this.txtTimKiem.Multiline = true;
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(200, 35);
+            this.txtTimKiem.TabIndex = 1;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnTimKiem.Image = ((System.Drawing.Image)(resources.GetObject("btnTimKiem.Image")));
+            this.btnTimKiem.Location = new System.Drawing.Point(218, 12);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(35, 35);
+            this.btnTimKiem.TabIndex = 6;
+            this.btnTimKiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTimKiem.UseVisualStyleBackColor = false;
+            // 
+            // TimKiem1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTimKiem);
+            this.Name = "TimKiem1";
+            this.Text = "TimKiem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+    }
+}
